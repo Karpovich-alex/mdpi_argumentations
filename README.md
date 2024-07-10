a8e328ea9135a76a09b9a4eec15d6efae0996265
karpovichalex <karpovehalex@gmail.com>
2024-07-10 13:23:00 +0300

    Add dataset and visualization function

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # mdpi_argumentations
+
+## Description
+
+- Total number of reviews: 115
+- Each review has been annotated two times by different annotators.
+- Total number of annotations: 164
+
+## Structure
+
+{article_review_name}.txt - file with collected text from all files from the review
+{article_review_name}_{reviewer}.tsv - file with annotation of the review
+
+### Annotation structure (*.tsv)
+
+Stores all sentences from the review. If sentence is not a review, then all columns will be None except _ann_ and _text_
+
+| Column name | Description                                                |
+|-------------|------------------------------------------------------------|
+| side        | side of the argument.                                      |
+| opponent    | opponent of the argument.                                  |
+| round       | Number of round                                            |
+| number      | Number of argument in the round                            |
+| attacks     | Number of attacking argument from the previous round       |
+| ann         | Type of argument (0-not an argument, 1-author, 2-reviewer) |
+| text        | Text of argument\sentence                                  |
+
+## Visualization
+
+You can use save_annotated_text_html func from [./src/utils.py](./src/utils.py)
+
+Example of result represented in [./assets/admsci5030125_boyarkin.html](./assets/admsci5030125_boyarkin.html)
+![visualization_example.png](./assets/visualization_example.png)
---
+++ src/utils.py
@@ -0,0 +1,52 @@
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+def save_annotated_text_html(path_to_ann, path_to_html, legend=None):
+    def colors_text(text, text_class):
+        return (f"<class-{text_class}>{text}</class-{text_class}>")
+
+    def gradient_colors(min_v, max_v):
+        cmap = plt.get_cmap("Pastel1")
+        norm = mcolors.Normalize(vmin=min_v, vmax=max_v)
+        return [mcolors.to_hex(cmap(norm(number))) for number in range(min_v, max_v + 1)]
+
+    df = pd.read_csv(path_to_ann, sep="\t", encoding="UTF-8")
+    texts, anns = df["text"].tolist(), df["ann"].tolist()
+    anns = np.array(anns)
+    unique_anns = np.unique(anns)
+    anns_colors = gradient_colors(1, len(unique_anns))
+
+    file = open(path_to_html, "w")
+
+    file.write("<html>")
+    color_classes = [f"class-{idx + 1} {{background-color: {c};}}" for idx, c in enumerate(anns_colors)]
+    color_classes.append("class-0 {background-color: #FFFFFF;}")
+    file.write(f"<style> html {{ font-family: \"Times New Roman\";}} {' '.join(color_classes)} </style>")
+    file.write("<div>Legend: ")
+
+    if legend is None:
+        legend = unique_anns
+    for idx, leg in enumerate(legend):
+        file.write(colors_text(leg, idx) + " ")
+    file.write(f"\tTotal classes num: {len(unique_anns)}")
+    file.write("</div><hr><div>")
+    for ann, text in zip(anns, texts):
+        file.write(colors_text(text+" ", ann))
+
+    file.write("</div></html>")
+    file.close()
+
+
+if __name__ == "__main__":
+    # save_annotated_text_html(
+    #     "./data/admsci5030125_boyarkin.tsv",
+    #     "./admsci5030125_boyarkin.html",
+    #     legend=["Обычный текст", "Аргумент автора", "Аргумент рецензента"])
+
+    save_annotated_text_html(
+        r"C:\Users\Karpo\PycharmProjects\diplom\mdpi_argumentations\data\admsci5030125_boyarkin.tsv",
+        "../assets/admsci5030125_boyarkin.html",
+        legend=["Обычный текст", "Аргумент автора", "Аргумент рецензента"])
