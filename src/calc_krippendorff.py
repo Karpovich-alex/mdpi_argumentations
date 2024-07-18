e51905b53e8e5716b41f3637b64b9a95c36776ff
karpovichalex <karpovehalex@gmail.com>
2024-07-18 16:53:38 +0300

    Correct annotations ...-ijerph19127332_perova

--- src/calc_krippendorff.py
+++ src/calc_krippendorff.py
@@ -1,9 +1,11 @@
+import csv
+
 import pandas as pd
 import krippendorff
 
 
 def get_annotation_by_words(path_to_ann):
-    df = pd.read_csv(path_to_ann, sep="\t", encoding="UTF-8")
+    df = pd.read_csv(path_to_ann, sep="\t", encoding="UTF-8", quoting=3)
     df["ann"] = df["ann"].apply(lambda x: x > 0).astype(int)
     texts, anns = df["text"].tolist(), df["ann"].tolist()
     words = []
@@ -17,13 +19,21 @@ def get_annotation_by_words(path_to_ann):
     return words, ann_arr
 
 
+def find_diffenrence(str1, str2):
+    for idx, el in enumerate(zip(str1, str2)):
+        el1, el2 = el
+        if el1 != el2:
+            return "#"+ str(idx)+ ":  "+el1 + "  ---  " + el2
+
+
 if __name__ == "__main__":
-    path_to_ann_1 = r".\data\admsci5030125_boyarkin.tsv"
+    path_to_ann_1 = r"C:\Users\Karpo\PycharmProjects\diplom\mdpi_argumentations\data\ijerph19127332_makarova.tsv"
     words_1, ann_arr_1 = get_annotation_by_words(path_to_ann_1)
 
-    path_to_ann_2 = r".\data\admsci5030125_makarova.tsv"
+    path_to_ann_2 = r"C:\Users\Karpo\PycharmProjects\diplom\mdpi_argumentations\data\ijerph19127332_perova.tsv"
     words_2, ann_arr_2 = get_annotation_by_words(path_to_ann_2)
 
-    assert len(words_1) == len(ann_arr_1) == len(words_2) == len(ann_arr_2), "lens are not equal"
+    assert len(words_1) == len(ann_arr_1) == len(words_2) == len(
+        ann_arr_2), f"lens are not equal: {len(words_1)} != {len(ann_arr_1)} != {len(words_2)}. first difference: {find_diffenrence(words_1, words_2)}"
 
     print(krippendorff.alpha(reliability_data=[ann_arr_1, ann_arr_2]))
